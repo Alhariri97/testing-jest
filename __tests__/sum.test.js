af1ba81b86ee0a1ffc056785f3c0af8ef6b85582
alhariri97 <19abdul97@gmail.com>
2022-04-13 23:52:13 +0100

    practicing tesing

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,9 +1,30 @@
 const theSum = require("../sum");
 
-test("Return the number", () => {
-  expect(theSum(10)).toBe(10);
-});
+describe("Test for numbers ", () => {
+  describe("test for one or zero argumments", () => {
+    test("My first test ", () => {
+      expect(theSum(1)).toBe(1);
+    });
+    test("My first test ", () => {
+      expect(theSum()).toBe(0);
+    });
+  });
 
-test(" 1 + 2 ", () => {
-  expect(theSum(10, 20)).toBe(30);
+  describe("test if two or three argg", () => {
+    test("if the user pass two arrguments, return the sum of the two arrgumnets ", () => {
+      expect(theSum(3, 2)).toBe(5);
+    });
+    test("My first test three  ", () => {
+      expect(theSum(1, 2, 3)).toBe(6);
+    });
+  });
+
+  describe("for more than two numbers ", () => {
+    test("if the user pass two arrguments, return the sum of the two arrgumnets ", () => {
+      expect(theSum(3, 2, 1, 4)).toBe(10);
+    });
+    test("if the user pass two arrguments, return the sum of the two arrgumnets ", () => {
+      expect(theSum(3, 2, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)).toBe(20);
+    });
+  });
 });
--- sum.js
+++ sum.js
@@ -1,5 +1,5 @@
-const theSum = (num, num2) => {
-  return num + (num2 || null);
+const theSum = (...num) => {
+  return num.reduce((a, b) => a + b, 0);
 };
 
 module.exports = theSum;
